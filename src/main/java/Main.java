f7d6bc92283f84fa32e822f53447ade6a21bda25
Dave Moten <davidmoten@gmail.com>
2015-07-25 11:17:14 +1000

    use java 8

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,5 +6,8 @@ public class Main {
 		Observable.just(1).subscribeOn(Schedulers.computation()).toBlocking()
 				.first();
 		System.out.println("finished");
+		Observable<Integer> o = Observable.just(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+		Observable<Integer> o2 = o.groupBy(i -> i % 5).concatMap(g -> g);
+
 	}
 }
