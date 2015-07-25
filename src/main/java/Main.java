fead436a58a0b990694e3e0a07f863d21af79a86
Dave Moten <davidmoten@gmail.com>
2015-07-26 01:24:29 +1000

    add transformer example with lambdas

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,50 @@
+import static rx.Observable.just;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import rx.Observable;
-import rx.schedulers.Schedulers;
+import rx.Observable.Transformer;
+import rx.Observer;
+import rx.functions.Action2;
+import rx.functions.Func0;
+import rx.functions.Func3;
+
+import com.github.davidmoten.rx.Transformers;
 
 public class Main {
-	public static void main(String[] args) throws InterruptedException {
-		Observable.just(1).subscribeOn(Schedulers.computation()).toBlocking()
-				.first();
-		System.out.println("finished");
-		Observable<Integer> o = Observable.just(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-		Observable<Integer> o2 = o.groupBy(i -> i % 5).concatMap(g -> g);
+    public static void main(String[] args) throws InterruptedException {
+        Observable<Integer> source = just(1, 1, 1, 2, 2, 3);
 
-	}
+        Func0<List<Integer>> initialState = () -> Collections.emptyList();
+
+        Func3<List<Integer>, Integer, Observer<List<Integer>>, List<Integer>> transition = (list,
+                n, observer) -> {
+            if (list.size() == 0)
+                return add(list, n);
+            else if (list.get(0).equals(n)) {
+                return add(list, n);
+            } else {
+                observer.onNext(list);
+                return Collections.singletonList(n);
+            }
+
+        };
+        Action2<List<Integer>, Observer<List<Integer>>> completionAction = (list, observer) -> {
+            if (list.size() > 0) {
+                observer.onNext(list);
+            }
+        };
+        Transformer<Integer, List<Integer>> transformer = Transformers
+                .<List<Integer>, Integer, List<Integer>> stateMachine(initialState, transition,
+                        completionAction);
+        System.out.println(source.compose(transformer).toList().toBlocking().single());
+    }
+
+    private static <T> List<T> add(List<T> list, T item) {
+        List<T> result = new ArrayList<T>(list);
+        result.add(item);
+        return result;
+    }
 }
