fa7e6cb64ad7a431b63e4c2ec4bb32f93afc05d2
Dave Moten <davidmoten@gmail.com>
2015-10-16 12:58:16 +1100

    dev

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,4 @@
-
 public class Main {
-	public static void main(String[] args) throws InterruptedException {
-
-	}
+    public static void main(String[] args) throws InterruptedException {
+    }
 }
