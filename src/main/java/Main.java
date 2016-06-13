28819fd760cb9ce722221659c8d8555b28467cfd
Dave Moten <davidmoten@gmail.com>
2016-06-13 18:52:51 +1000

    subscribeOn

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,23 @@
-import java.io.File;
-import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
 
-import org.mapdb.DB;
-import org.mapdb.DBMaker;
-import org.mapdb.Serializer;
+import rx.Observable;
+import rx.schedulers.Schedulers;
 
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
-        System.out.println(-1 >> 16);
-        DB db = DBMaker.newFileDB(new File("target/db")).make();
-        BlockingQueue<String> fifo = db.createQueue("fifo", Serializer.STRING, false);
-        fifo.add("hello");
-        fifo.add("boo");
-        String x = null;
-        while ((x = fifo.poll()) != null) {
-            System.out.println(x);
-        }
-    }
 
+        Observable<Integer> o = Observable.range(1, Integer.MAX_VALUE)
+                // .doOnRequest(n -> System.out.println("requested b " + n)) //
+                .onBackpressureBuffer() //
+                // .doOnRequest(n -> System.out.println("requested a " + n)) //
+                .subscribeOn(Schedulers.io()) //
+                .subscribeOn(Schedulers.io()) //
+        // .doOnRequest(n -> System.out.println("requested m " + n)) //
+        ; //
+        o.doOnNext(System.out::println) //
+                .count() //
+                .timeout(5, TimeUnit.SECONDS) //
+                .toBlocking().single();
+    }
 }
